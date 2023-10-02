fc5370148c850a11b740790fa1bc517c2c010386
Evie Hoang <evie.h0325@gmail.com>
2023-10-02 02:09:20 -0700

    Centered text in HTML and CSS. Removed unused ids.

--- assets/js/script.js
+++ assets/js/script.js
@@ -104,103 +104,103 @@ function showQuestion(n) {
     n = qNumber;
 };
 
-// // If answer is right or wrong, display.
-// function check(event) {
-//     // display checked answer
-//     event.preventDefault();
-//     checkArea.style.display = "block";
-//     // how long display last
-//     setTimeout(function () {
-//         checkArea.style.display = "none";
-//     }, 1000);
+// If answer is right or wrong, display.
+function check(event) {
+    // display checked answer
+    event.preventDefault();
+    checkArea.style.display = "block";
+    // how long display last
+    setTimeout(function () {
+        checkArea.style.display = "none";
+    }, 1000);
 
-//     // actual checking
-//     if (qs[qNumber].answers == event.target.value) {
-//         checkArea.textContent = "Yay!";
-//         score = score + 1;
-//     }
-//     else {
-//         secondsLeft = secondsLeft - 5;
-//         checkArea.textContent = "Opps!";
-//     }
-//     if (qNumber < qs.length - 1) {
-//         // if question number is less than the length still, get a new question.
-//         showQuestions(qNumber + 1)
-//     }
-//     else {
-//         gameOver();
-//     }
-//     qCount++;
-// };
+    // actual checking
+    if (qs[qNumber].answers == event.target.value) {
+        checkArea.textContent = "Yay!";
+        score = score + 1;
+    }
+    else {
+        secondsLeft = secondsLeft - 5;
+        checkArea.textContent = "Opps!";
+    }
+    if (qNumber < qs.length - 1) {
+        // if question number is less than the length still, get a new question.
+        showQuestions(qNumber + 1)
+    }
+    else {
+        gameOver();
+    }
+    qCount++;
+};
 
-// // Store and get scores
-// function getScore() {
-//     var currentScore = localStorage.getItem("Points");
-//     if (currentScore !== null) {
-//         points = JSON.parse(currentScore);
-//         return points;
-//     }
-//     else {
-//         score = [];
-//     }
-//     return score;
-// };
+// Store and get scores
+function getScore() {
+    var currentScore = localStorage.getItem("Points");
+    if (currentScore !== null) {
+        points = JSON.parse(currentScore);
+        return points;
+    }
+    else {
+        score = [];
+    }
+    return score;
+};
 
-// // show the score
-// function showScore() {
-//     scoreArea.innerHTML = "";
-//     scoreArea.style.display = "block";
-//     var topScore = sort();
-//     var topThree = topScore.slice(0, 3);
-//     for (var i = 0; i < topThree.length; i++) {
-//         var r = topThree[i];
-//         var li = document.createElement("li");
-//         li.textContent = r.user + " - " + r.score;
-//         li.setAttribute("score-area", i);
-//         scoreArea.appendChild(li);
-//     }
-// };
+// show the score
+function showScore() {
+    scoreArea.innerHTML = "";
+    scoreArea.style.display = "block";
+    var topScore = sort();
+    var topThree = topScore.slice(0, 3);
+    for (var i = 0; i < topThree.length; i++) {
+        var r = topThree[i];
+        var li = document.createElement("li");
+        li.textContent = r.user + " - " + r.score;
+        li.setAttribute("score-area", i);
+        scoreArea.appendChild(li);
+    }
+};
 
-// // Sort score by highest point and display list
-// function sort() {
-//     var list = getScore();
-//     if (getScore == null) {
-//         return;
-//     }
-//     else {
-//         list.sort(function (a, b) {
-//             return b.score - a.score;
-//         }
-//         )
-//         return list;
-//     }
-// };
+// Sort score by highest point and display list
+function sort() {
+    var list = getScore();
+    if (getScore == null) {
+        return;
+    }
+    else {
+        list.sort(function (a, b) {
+            return b.score - a.score;
+        }
+        )
+        return list;
+    }
+};
 
-// function updatedList(e) {
-//     var addList = getScore();
-//     addList.push(e);
-//     localStorage.setItem("Points", JSON.stringify(addList));
-// };
+function updatedList(e) {
+    var addList = getScore();
+    addList.push(e);
+    localStorage.setItem("Points", JSON.stringify(addList));
+};
 
-// function save() {
-//     var rank = {
-//         user: userInitial.value,
-//         score: score,
-//     }
-//     addItem(rank);
-//     getScore();
-// }
+function save() {
+    var rank = {
+        user: userInitial.value,
+        score: score,
+    }
+    addItem(rank);
+    getScore();
+}
 
-// // if game ends before answering all questions correctly or if timed out. Game ends.
-// function gameOver() {
-//     questions.style.display = "none";
-//     score.style.display = "block";
-//     scoreTotal.textContent = "Your score is: " + total;
-//     timeLeft.style.display = "none";
-// };
+// if game ends before answering all questions correctly or if timed out. Game ends.
+function gameOver() {
+    questions.style.display = "none";
+    score.style.display = "block";
+    scoreTotal.textContent = "Your score is: " + total;
+    timeLeft.style.display = "none";
+};
 
 
-// // Event listeners
+// Event listeners
 
 goBtn.addEventListener("click", start);
 // answer1.addEventListener("click", chooseA);
